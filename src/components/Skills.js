46bdc1b3268f5fcc00fb1e79bfd98aef8086a22c
ajzajac <ajzajac@icloud.com>
2020-07-04 08:43:40 -0400

    updated meta data

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -5,6 +5,10 @@ export default function Skills() {
         <div className='skillsPage' id='skills'>
             <h2 style={{marginTop: '-1%', paddingTop: '2%'}}>Skills</h2>
             <div>
+            <div>
+            <i className="devicon-javascript-plain"></i>
+            <p>Javascript</p>
+          </div>
                 <p style={{marginBottom: '-1%', paddingBottom: '2%'}}>Skills go here</p>
             </div>
         </div>
